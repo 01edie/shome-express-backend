82761a374a9bea0a9d919eb873a50b12c6253c6b
BISWAJIT BOSE <97077410+01edie@users.noreply.github.com>
2025-01-13 10:54:07 +0530

    minor changes for deployment

--- dist/server.js
+++ dist/server.js
@@ -8,14 +8,20 @@ const database_1 = require("./db/database");
 const dotenv_1 = __importDefault(require("dotenv"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
+const helmet_1 = __importDefault(require("helmet"));
 const logger_service_1 = __importDefault(require("./services/logger.service"));
 const index_router_1 = __importDefault(require("./routes/v1/index.router"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:4173", "http://localhost:5173", "https://app-shome.vercel.app/"],
+    origin: [
+        "http://localhost:4173",
+        "http://localhost:5173",
+        "https://app-shome.vercel.app/",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
@@ -24,7 +30,7 @@ database_1.sequelize
     .authenticate()
     .then(() => {
     console.log("Database connected!");
-    app.listen(process.env.SERVER_PORT, () => console.log(`Server running on SERVER_PORT : ${process.env.SERVER_PORT}`));
+    app.listen(+process.env.SERVER_PORT, () => console.log(`Server running on SERVER_PORT : ${process.env.SERVER_PORT}`));
     process.on("uncaughtException", (err) => {
         var _a;
         logger_service_1.default.error(`Uncaught Exception: ${(_a = err.stack) !== null && _a !== void 0 ? _a : err.message}`);
