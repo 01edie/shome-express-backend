bb1c973f68b76cfd49269df1238263ff7addcf72
BISWAJIT BOSE <97077410+01edie@users.noreply.github.com>
2025-01-13 10:51:44 +0530

    Delete dist directory

--- dist/config/access-control.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenConfig = void 0;
-exports.tokenConfig = {
-    accessTokenExpiresIn: 3600, // 1h
-    refreshTokenExpiresIn: 259200, // 3d
-};
--- dist/utils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.boolInt = void 0;
-const boolInt = (b) => (b ? 1 : 0);
-exports.boolInt = boolInt;
